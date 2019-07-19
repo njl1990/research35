302f6c5c8e6314bb03fd670940eb0f33e14d8e19
Bowen <bowen.nan@icloud.com>
2019-07-19 10:40:54 +0800

    add

--- setup.sh
+++ setup.sh
@@ -12,4 +12,4 @@ sudo docker rm research35.db
 sudo docker rm research35.web
 # run docker 
 sudo docker run --name=research35.db -p 0.0.0.0:27017:27017 -v /usr/local/research35/data/db:/data/db -d mongo
-sudo docker run --name=research35.web --link=research35.db -d -p 0.0.0.0:8888:80 bowen/research35.web 'python3' '/usr/local/research35/web/manage.py' 'runserver' '0.0.0.0:80'
+sudo docker run --name=research35.web --link=research35.db -d -p 0.0.0.0:80:80 bowen/research35.web 'python3' '/usr/local/research35/web/manage.py' 'runserver' '0.0.0.0:80'
